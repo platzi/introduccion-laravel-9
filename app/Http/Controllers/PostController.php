658cabebb0eabcd6cf3195dfcec7e4dca3b6ebbd
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 17:36:04 -0500

    panel administrativo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() 
+    {
+        return view('posts.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 Route::controller(PageController::class)->group(function () {     
 
@@ -14,5 +15,7 @@ Route::controller(PageController::class)->group(function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('posts', PostController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
