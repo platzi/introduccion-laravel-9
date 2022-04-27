373c126830df1bf17c3fd17383795690486ad450
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 18:40:04 -0500

    listado de publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() 
     {
-        return view('posts.index');
+    	return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,13 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Posts') }}</h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        	{{ __('Posts') }}
+       	</h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                	Listado de publicaciones
+                	
+                	<table class="mb-4">
+                        @foreach($posts as $post)
+                        <tr class="border-b border-gray-200 text-sm">
+                            <td class="px-6 py-4">{{ $post->title }}</td>
+                            <td class="px-6 py-4">
+                                <a href="#" class="text-indigo-600">Editar</a>
+                            </td>
+                            <td class="px-6 py-4">
+                            	Eliminar
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+
+                    {{ $posts->links() }}
                 </div>
             </div>
         </div>
