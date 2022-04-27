9b6c343a8dfa6d92d3a335670bc2646658224447
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 14:27:45 -0500

    views

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route::get()    |  Consultar
- * Route::post()   |  Guardar
- * Route::delete() |  Eliminar
- * Route::put()    |  Actualizar
- */
-
 Route::get('/', function () {
-    return 'Ruta home';
+    return view('home');
 });
 
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    // consulta en base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('blog/{slug}', function ($slug) {
-    // consulta en base de datos
+    // consulta en base de datos con el slug
+    $post = $slug;
 
-    return $slug;
-});
-
-Route::get('buscar', function (Request $request) {
-    return $request->all();
+    return view('post', ['post' => $post]);
 });
