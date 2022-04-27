c46dd87242240d3d4e3fc55aabccd412bcf620dc
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 14:10:17 -0500

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Route::get()    |  Consultar
+ * Route::post()   |  Guardar
+ * Route::delete() |  Eliminar
+ * Route::put()    |  Actualizar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // consulta en base de datos
+
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
