81a6e6d01280d92a647d03b7597d37b126b0b51e
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 20:30:34 -0500

    diseño personalizado

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -27,7 +27,22 @@
 			@endif
 		</header>
 
+		<div class="opacity-60 h-px mb-8" style="
+			background: linear-gradient(to right, 
+				rgba(200, 200, 200, 0) 0%,
+				rgba(200, 200, 200, 1) 30%,
+				rgba(200, 200, 200, 1) 70%,
+				rgba(200, 200, 200, 0) 100%
+			);
+		">
+			
+		</div>
+
 		@yield('content')
+
+		<p class="py-16">
+			<img src="{{ asset('images/logo.png') }}" class="h-12 mx-auto">
+		</p>
 	</div>
 </body>
 </html>
