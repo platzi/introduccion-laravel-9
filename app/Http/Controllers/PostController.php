c270a6865c9c01ca523b8180ec4478d071e9a26e
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 19:29:48 -0500

    función de guardar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -17,6 +19,17 @@ class PostController extends Controller
     public function create(Post $post) 
     {
         return view('posts.create', compact('post'));
+    }
+
+    public function store(Request $request) 
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug'  => Str::slug($title),
+            'body'  => $request->body,
+        ]);
+
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post) 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,12 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+    	'title', 
+    	'slug', 
+    	'body'
+    ];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
