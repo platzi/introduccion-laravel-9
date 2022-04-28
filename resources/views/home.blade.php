037026a4743c9236647ebd55578ba8314d89a415
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 21:03:01 -0500

    publicación individual

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
 
     <div class="grid grid-cols-1 gap-4 mb-4">
     	@foreach($posts as $post)
-        <a href="" class="bg-gray-100 rounded-lg px-6 py-4">        
+        <a href="{{ route('post', $post->slug) }}" class="bg-gray-100 rounded-lg px-6 py-4">        
             <p class="text-xs flex items-center gap-2">
                 <span class="uppercase text-gray-700 bg-gray-200 rounded-full px-2 py-1">Tutorial</span>
                 <span>{{ $post->created_at->format('d/m/Y') }}</span>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,12 @@
 @extends('template')
 	
 @section('content')
-	
-<h1>{{ $post->title }}</h1>
-<p>
-	{{ $post->body }}
-</p>
+
+<div class="max-w-3xl mx-auto">
+	<h1 class="text-5xl mb-8">{{ $post->title }}</h1>
+	<p class="leading-loose text-lg text-gray-700">
+		{{ $post->body }}
+	</p>
+</div>
 
 @endsection
