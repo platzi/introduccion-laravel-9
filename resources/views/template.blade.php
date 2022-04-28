6a32fc76a0ae4419862ad76c38d544288d4f9b9d
italomoralesf <profesoritalomoralesf@gmail.com>
2022-04-27 20:13:45 -0500

    diseño web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,21 +5,29 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Proyecto web</title>
+
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-	<p>
-		<a href="{{ route('home') }}">Home</a>
-		<a href="{{ route('blog') }}">Blog</a>
+	<div class="container px-4 mx-auto">
+		<header class="flex justify-between items-center py-4">
+			<div class="flex items-center flex-grow gap-4">
+				<a href="{{ route('home') }}">
+					<img src="{{ asset('images/logo.png') }}" class="h-12">
+				</a>
+				<form action="">
+					<input type="text" placeholder="Buscar">
+				</form>
+			</div>
 
-		@auth
-		<a href="{{ route('dashboard') }}">Dashboard</a>
-		@else
-		<a href="{{ route('login') }}">Login</a>
-		@endif
-	</p>	
+			@auth
+			<a href="{{ route('dashboard') }}">Dashboard</a>
+			@else
+			<a href="{{ route('login') }}">Login</a>
+			@endif
+		</header>
 
-	<hr>
-	
-	@yield('content')
+		@yield('content')
+	</div>
 </body>
 </html>
